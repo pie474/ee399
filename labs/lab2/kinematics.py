5c08978a709f15d8c8634ea6a87b8810d730d65e
pie474 <chinmaymurthy474@gmail.com>
2024-10-25 20:20:27 -0700

    clean up kinematics module

--- labs/lab2/kinematics.py
+++ labs/lab2/kinematics.py
@@ -1,24 +1,26 @@
 """
 Defines all kinematic methods we may use repeatedly over time.
 
-All units are in mm.
+All units are mm or radians.
 """
 import sympy as sp
 from sympy import symbols, cos, sin, pi, simplify, pprint, Matrix
 
 
-def get_transformation_matrix(a, alpha, d, theta):
-    return Matrix([[cos(theta), -sin(theta) * cos(alpha), sin(theta) * sin(alpha), a * cos(theta)],
-                [sin(theta), cos(theta) * cos(alpha), -cos(theta) * sin(alpha), a * sin(theta)],
-                [0, sin(alpha), cos(alpha), d],
-                [0, 0, 0, 1]])
+def dh_row_to_transformation(a, alpha, d, theta):
+    return Matrix([
+        [cos(theta), -sin(theta) * cos(alpha),  sin(theta) * sin(alpha), a * cos(theta)],
+        [sin(theta),  cos(theta) * cos(alpha), -cos(theta) * sin(alpha), a * sin(theta)],
+        [0,                        sin(alpha),               cos(alpha),              d],
+        [0,                                 0,                                     0, 1]])
 
 
 # a values for each link given in mm
 a1, a2, a3, a4, a5, a6 = 0, 100, 0, 0, 0, 0
 
 # d values for each link given in mm
-d1, d2, d3, d4, d5, d6 = 136, 0, 0, 107, 0, 65
+# d1, d2, d3, d4, d5, d6 = 136, 0, 0, 107, 0, 65  # obtained via datasheets
+d1, d2, d3, d4, d5, d6 = 120, 0, 0, 100, 0, 50  # obtained experimentally
 
 # joint angles, left symbolic so they can be substituted later
 q1, q2, q3, q4, q5, q6 = symbols('q1 q2 q3 q4 q5 q6')
@@ -33,11 +35,10 @@ DH_TABLE = [
     [a6,     0, d6, q6]   # J6 (End effector)
 ]
 
-# Define the offset parameters
 JOINT_ANGLE_OFFSETS = [0, -pi/2, 0, 0, 0, 0]
 
-T0EE = sp.prod(get_transformation_matrix(ai, alphai, di, thetai) for ai, alphai, di, thetai in DH_TABLE)
-# T0EE = simplify(T0EE)  # takes like 5 seconds to run
+T_FORWARD = sp.prod(dh_row_to_transformation(ai, alphai, di, thetai) for ai, alphai, di, thetai in DH_TABLE)
+# T_FORWARD = simplify(T_FORWARD)  # takes ~5 seconds to run, uncomment only if needed
 
 def get_translation(transformation):
     return transformation[:3, 3]
@@ -57,29 +58,5 @@ def compute_forward_matrix(joint_angles):
     subs_dict = {q:offset + angle for q, offset, angle in zip([q1, q2, q3, q4, q5, q6], JOINT_ANGLE_OFFSETS, joint_angles)}
 
     # Substitute joint angles into the final transformation matrix
-    return simplify(T0EE.subs(subs_dict))
+    return simplify(T_FORWARD.subs(subs_dict))
 
-
-def cyclic_coordinate_descent(T0EE_d, curr_joint_angles, state_func, error_threshold=1, max_iterations=1000):
-    """Implements the Cyclic Coordinate Descent (CCD) algorithm to numerically find a solution to the 
-        inverse kinematics problem given by the target base-EE transformation
-
-    """
-    state_d = state_func(T0EE_d)
-    joint_angles = curr_joint_angles.copy()
-
-    def error(ja):
-        return (state_func(compute_forward_matrix(ja)) - state_d).norm()
-
-    while(error(joint_angles) > error_threshold):
-        ...
-
-
-if __name__ == '__main__':
-    Td_test = Matrix([[1, 0, 0, 100],
-                    [0, 1, 0, 100],
-                    [0, 0, 1, 100],
-                    [0, 0, 0, 1]])
-
-
-    pprint(get_pose_ts(Td_test).norm())
