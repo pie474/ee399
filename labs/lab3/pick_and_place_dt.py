1ecf6f2f2ea0b6e22e246f3c565d0acdbd89ae7e
pie474 <chinmaymurthy474@gmail.com>
2024-11-21 12:01:20 -0800

    dt pp works

--- labs/lab3/pick_and_place_dt.py
+++ labs/lab3/pick_and_place_dt.py
@@ -1,4 +1,4 @@
-from pymycobot import MechArm, PI_PORT, PI_BAUD, MyCobotSocket
+from pymycobot import MyCobotSocket
 from time import sleep
 
 STRAIGHT_UP = [0, 0, -90, 0, 0, 0]
@@ -7,7 +7,15 @@ HOME = [0, 0, 0, 0, 0, 0]
 
 CENTER = [-4.74, 26.27, -64.16, 13.79, 90.52, 75.84]
 all_angles = [
-    
+    [-27.5, 119.88, -90.52, -7.64, 49.13, -34.54],
+    [-5.44, 81.91, -90.52, 6.32, 78.75, -31.11],
+    [17.75, 119.53, -90.43, 5.97, 56.42, -31.11]    
+]
+
+all_angles_elevated = [
+    [-16.61, 74.53, -58.0, -13.71, 63.36, 2.02],
+    [-2.37, 48.07, -58.0, -1.31, 79.8, -0.43],
+    [25.75, 74.44, -58.0, -0.61, 69.43, 5.71]
 ]
 
 def follow_angle_path(arm, angles, speed=30, sleep_time=2):
@@ -30,11 +38,11 @@ def main():
     sleep(2)
 
     for i in range(1):
-        follow_angle_path(arm, all_angles, speed=50, sleep_time=0.3)
+        follow_angle_path(arm, all_angles_elevated, speed=50, sleep_time=1)
 
         # actual_points.append(([], []))
 
-    arm.send_angles(STRAIGHT_UP, 30)
+    arm.send_angles(HOME, 30)
 
 
 
