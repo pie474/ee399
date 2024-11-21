41e80e76be45bb47b3000b6af037429ef42e6f0b
pie474 <chinmaymurthy474@gmail.com>
2024-11-21 11:49:38 -0800

    ugh

--- labs/lab3/pick_and_place.py
+++ labs/lab3/pick_and_place.py
@@ -2,7 +2,16 @@ from time import sleep
 from pymycobot import MyCobotSocket
 from kinematics import *
 
+STRAIGHT_UP = [0, 0, -90, 0, 0, 0]
 
+def get_angles(arm):
+    angles = None
+    while not angles or angles == -1:
+        try:
+            angles = arm.get_angles()
+        except:
+            print('retrying angle getter')
+    return angles
 
 def main():
     ip_addr = "10.19.108.58"
@@ -11,12 +20,21 @@ def main():
 
     sleep(2)
 
-    q_init = arm.get_angles()
+    arm.sync_send_angles(STRAIGHT_UP, 60)
+
+    sleep(2)
+
+    q_init = get_angles(arm)
     q_init = [0, 0, 0, 0, 0, 0]
 
-    ik = inverse_kinematics(150,0,224, 0,0,0,q_init)
+    sleep(2)
 
-    arm.send_angles(list(ik), 40)
+    ik = inverse_kinematics(150,0,224, 0,0,0,q_init).tolist()
+
+    arm.send_angles(ik, 40)
+
+    # arm.send_angles([0, 0, 0, 0, 0, 0], 60)
+
 
 
 
